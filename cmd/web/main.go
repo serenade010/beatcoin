25716410fed1bc2cacd91c835a8ae7d982908710
serenade010 <lukechu010@gmail.com>
2022-10-22 22:46:03 +0800

    add TLS config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"html/template"
@@ -66,10 +67,17 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	// Create non-default TLS settings.
+	// Only elliptic curves with assembly implementations are used.
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	srv := &http.Server{
-		Addr:     *addr,
-		ErrorLog: errorlog,
-		Handler:  app.routes(),
+		Addr:      *addr,
+		ErrorLog:  errorlog,
+		Handler:   app.routes(),
+		TLSConfig: tlsConfig,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
